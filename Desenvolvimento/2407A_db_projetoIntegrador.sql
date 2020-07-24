2e50003c66d6a2b5494c052a4fd2b9c0964fe1a4
unknown <moniquejroliveira@gmail.com>
2020-07-24 17:58:30 -0300

    Começando database do projeto

--- Desenvolvimento/2407A_db_projetoIntegrador.sql
+++ Desenvolvimento/2407A_db_projetoIntegrador.sql
@@ -1,29 +1,28 @@
-create database db_projeto_Integrador;
-use db_projeto_Integrador;
+create database db_projeto_integrador;
 
+
+ CREATE TABLE tb_categoria ( 
+ id bigint NOT NULL auto_increment, 
+ nome varchar(255) NOT NULL,
+ setor varchar(255) NOT NULL,
+ PRIMARY KEY (id) 
+ ); 
+
+CREATE TABLE tb_produto ( 
+id bigint NOT NULL AUTO_INCREMENT,
+ nome varchar(255) NOT NULL, 
+ preco DECIMAL(6,2) NOT NULL, 
+ quantidade DECIMAL(6,2),
+ categoria_id bigint,
+ PRIMARY KEY (id),
+ FOREIGN KEY (categoria_id) REFERENCES tb_categoria(id)
+ ); 
+ 
 CREATE TABLE tb_usuario (
-	id INT NOT NULL AUTO_INCREMENT,
-	nome varchar(255) NOT NULL,
-	email varchar(255) NOT NULL,
-	senha varchar(8) NOT NULL, 
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE tb_produto (
-	id INT NOT NULL AUTO_INCREMENT,
-	categoria_id INT NOT NULL,
-	nome varchar(255) NOT NULL,
-	preco DECIMAL(6,2) NOT NULL,
-	quantidade DECIMAL(6,2) NOT NULL,
-	disponivel BOOLEAN NOT NULL,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE tb_categoria (
-	id INT NOT NULL AUTO_INCREMENT,
-	nome varchar(255) NOT NULL,
-	setor varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-);
-
-ALTER TABLE tb_categoria ADD CONSTRAINT tb_categoria_fk0 FOREIGN KEY (id) REFERENCES tb_produto(categoria_id);
+ id bigint NOT NULL AUTO_INCREMENT, 
+ nome varchar(255) NOT NULL,
+ email varchar(255) NOT NULL, 
+ senha varchar(8) NOT NULL,
+ PRIMARY KEY (id) 
+ ); 
+ 
