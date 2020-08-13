21510578911df5d929a652a05fd3c51a806f5c53
unknown <moniquejroliveira@gmail.com>
2020-08-13 20:09:08 -0300

    atualização

--- docampopramesa/src/main/java/com/generation/docampopramesa/model/Categoria.java
+++ docampopramesa/src/main/java/com/generation/docampopramesa/model/Categoria.java
@@ -1,4 +1,4 @@
-package com.generation.docampopramesa.model;
+package br.com.generation.docampoparamesa.model;
 
 import java.util.List;
 
@@ -6,7 +6,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
@@ -15,33 +14,24 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
-@Table(name = "categoria")
+@Table(name = "tb_categoria")
 public class Categoria {
-	
-	@Id
+
+	@javax.persistence.Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
 	@Size(min = 5, max = 255)
 	private String nome;
-	
+
 	@NotNull
 	@Size(min = 5, max = 255)
 	private String setor;
-	
+
 	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
 	@JsonIgnoreProperties("categoria")
 	private List<Produto> produto;
-	
-
-	public List<Produto> getProduto() {
-		return produto;
-	}
-
-	public void setProduto(List<Produto> produto) {
-		this.produto = produto;
-	}
 
 	public long getId() {
 		return id;
@@ -65,6 +55,14 @@ public class Categoria {
 
 	public void setSetor(String setor) {
 		this.setor = setor;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 }
